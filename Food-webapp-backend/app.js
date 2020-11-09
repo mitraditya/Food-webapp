5fa38b73e551b983da209f4dc3a96671b5eefcd3
Adityamitra <adityamitra1911@gmail.com>
2020-11-09 17:58:27 +0530

    Basic Working Project

--- Food-webapp-backend/app.js
+++ Food-webapp-backend/app.js
@@ -23,16 +23,48 @@ app.post('/foodList', async (req, res) =>{
     res.redirect('/')
 })
 
+app.post('/favFoodList', async (req, res) =>{
+    foodList.findById(mongoose.Types.ObjectId(req.query.id), async function (err, doc){
+        if(err){
+            console.log(err)
+            res.send("Not found")
+        }else{
+        favFoodList.exists({ oldid: doc._id }, async function(err, result) {
+            if (err) {
+                console.log(err)
+                res.send("Error")
+            } else {
+                if(result==false){
+                    //create new entry in favFoodList
+                    await favFoodList.create({ oldid: doc._id, item: doc.item, cuisine: doc.cuisine})
+                    res.send("Entry created")
+                }
+                else{
+                    //return saying that it is already added to favourites
+                    console.log("Already exists");
+                    res.send("Already exists");
+                }
+            }
+          });
+    }
+      });
+})
+
 app.get('/foodList', async (req, res) => {
     const fList = await foodList.find()
     res.json({ fList: fList});
+});
+
+app.get('/favFoodList', async (req, res) => {
+    const favList = await favFoodList.find()
+    res.json({ favList: favList});
 });
 
 app.delete('/deleteitem', async (req, res) => {
     await foodList.findOneAndRemove({ _id: mongoose.Types.ObjectId(req.query.id)},function (err) {
         if(err){
             console.log(err)
-            res.send("nahi hai maal")
+            res.send("Not found")
         }else{
         console.log("Successful deletion");
         res.send("Successfully Deleted")}
