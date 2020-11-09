c70607b2c9e35c62f4282b4a8e423568ec1a9cb3
Adityamitra <adityamitra1911@gmail.com>
2020-11-09 16:22:01 +0530

    Added dynamic cards

--- Food-webapp-backend/app.js
+++ Food-webapp-backend/app.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const app = express()
-const favFoodList = require('./favfoodlist')
+var cors = require('cors')
+var app = express()
+
+app.use(cors())
+const foodList = require('./foodlist')
 
 mongoose.connect('mongodb://localhost/favfoodlist', {
     useNewUrlParser: true, useUnifiedTopology: true
@@ -10,31 +13,30 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 
 app.get('/', async (req, res) =>{
-    const favList = await favFoodList.find()
-    res.json({message: 'Hello World', favList: favList});
+    const fList = await foodList.find()
+    res.json({message: 'Hello World', fList: fList});
 })
 
-app.post('/favFoodList', async (req, res) =>{
-    await favFoodList.create({ item: req.body.item})
+app.post('/foodList', async (req, res) =>{
+    await foodList.create({ item: req.body.item, description: req.body.description, cuisine: req.body.cuisine})
     res.redirect('/')
 })
 
-app.get('/favFoodList', async (req, res) => {
-    const favList = await favFoodList.find()
-    res.json({ favList: favList});
+app.get('/foodList', async (req, res) => {
+    const fList = await foodList.find()
+    res.json({ fList: fList});
 });
 
-app.delete('/deleteitem/:id', async (req, res) => {
-    await favFoodList.deleteOne({ _id: req.params.id}).then(err => {
-        if(err) {
+app.delete('/deleteitem', async (req, res) => {
+    await foodList.findOneAndRemove({ _id: mongoose.Types.ObjectId(req.query.id)},function (err) {
+        if(err){
             console.log(err)
-            res.send("error")
-        }
-        res.send("Successfully deleted")
-        console.log("Deleted")
-    })
-
-})
+            res.send("nahi hai maal")
+        }else{
+        console.log("Successful deletion");
+        res.send("Successfully Deleted")}
+      }
+)})
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`))
